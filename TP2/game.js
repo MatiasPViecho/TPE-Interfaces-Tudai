f926af1d6d2cbf92d128719a2fc7b7fb3a02a3a0
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-10-09 01:22:55 -0300

    complete share popup

--- TP2/game.js
+++ TP2/game.js
@@ -9,6 +9,9 @@ shareCard
   .querySelector(".close")
   .addEventListener("click", () => shareCard.classList.remove("show"));
 
+  shareCard
+  .querySelectorAll(".share-with svg").forEach(element => element.addEventListener("click", () => shareCard.classList.remove("show")));
+
 shareCard.addEventListener("click", (event) => {
   if (event.target == shareCard.querySelector(".share-card-background")) {
     shareCard.classList.remove("show");
