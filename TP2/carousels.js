7d909a55e3bba10ea267e53d385dfd6895c9641c
MatiasPViecho <mpviecho@gmail.com>
2023-10-08 21:12:19 -0300

    feat(Carousel): add interval to scroll right and left, add to all containers

--- TP2/carousels.js
+++ TP2/carousels.js
@@ -2,45 +2,49 @@ const btnRightElements = document.querySelectorAll('.btn-right');
 const btnLeftElements = document.querySelectorAll('.btn-left');
 
 btnRightElements.forEach((btn) => {
+  setInterval(() => {
+    handleScrollRight(btn);
+  }, 3500);
   btn.addEventListener('click', () => {
-    console.log('clicked right btn');
-    let parent = btn.closest('.relative');
-    let container = parent.querySelector('.cards');
-    let items = container.childElementCount;
-    let itemWidth = container.firstElementChild.getBoundingClientRect().width;
-    let gap = Number(getComputedStyle(container).gap.replace('px', ''));
-    let nextScrollPos = itemWidth + gap;
-    const itemsInView = Math.floor(container.offsetWidth / itemWidth);
-    console.log(container.scrollLeft);
-    console.log(itemsInView);
-    console.log((items - 1) * itemWidth - items * gap);
-    let scrollAmount =
-      itemsInView > 1
-        ? (container.scrollLeft * itemsInView) / itemsInView
-        : container.scrollLeft;
-    console.log('scrollAmount: ', scrollAmount);
-    if (scrollAmount >= (items - 1) * itemWidth - items * gap) {
-      container.scrollBy(-container.scrollLeft, 0);
-    } else {
-      container.scrollBy(nextScrollPos, 0);
-    }
+    handleScrollRight(btn);
   });
 });
 
 btnLeftElements.forEach((btn) => {
   btn.addEventListener('click', () => {
-    console.log('clicked left btn');
-    let parent = btn.closest('.relative');
-    let container = parent.querySelector('.cards');
-    let items = container.childElementCount;
-    let itemWidth = container.firstElementChild.getBoundingClientRect().width;
-    let gap = Number(getComputedStyle(container).gap.replace('px', ''));
-    let nextScrollPos = itemWidth + gap;
-    if (container.scrollLeft < itemWidth) {
-      console.log('if');
-      container.scrollBy(nextScrollPos * items, 0);
-    } else {
-      container.scrollBy(-(nextScrollPos + gap), 0);
-    }
+    handleScrollLeft(btn);
   });
 });
+
+const handleScrollRight = (btn) => {
+  let parent = btn.closest('.relative');
+  let container = parent.querySelector('.cards');
+  let items = container.childElementCount;
+  let itemWidth = container.firstElementChild.getBoundingClientRect().width;
+  let gap = Number(getComputedStyle(container).gap.replace('px', ''));
+  let nextScrollPos = itemWidth + gap;
+  const itemsInView = Math.floor(container.offsetWidth / itemWidth);
+  if (
+    container.scrollLeft * itemsInView >=
+    (items - 1) * itemWidth - items * gap
+  ) {
+    container.scrollBy(-container.scrollLeft, 0);
+  } else {
+    container.scrollBy(nextScrollPos + gap * itemsInView, 0);
+  }
+};
+const handleScrollLeft = (btn) => {
+  let parent = btn.closest('.relative');
+  let container = parent.querySelector('.cards');
+  let items = container.childElementCount;
+  let itemWidth = container.firstElementChild.getBoundingClientRect().width;
+  let gap = Number(getComputedStyle(container).gap.replace('px', ''));
+  const itemsInView = Math.floor(container.offsetWidth / itemWidth);
+  let nextScrollPos = itemWidth + gap;
+
+  if (container.scrollLeft < itemWidth) {
+    container.scrollBy(nextScrollPos * items, 0);
+  } else {
+    container.scrollBy(-(nextScrollPos + gap * itemsInView), 0);
+  }
+};
