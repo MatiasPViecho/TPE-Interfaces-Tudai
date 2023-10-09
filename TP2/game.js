fc1253f9552f2b934b38dcceb6cb21d24e5b65bd
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-10-09 00:50:14 -0300

    changes in game page WIP

---
+++ TP2/game.js
@@ -0,0 +1,18 @@
+document.querySelectorAll(".btn-favourite").forEach((btn) => {
+  btn.addEventListener("click", () => btn.classList.toggle("not-like"));
+});
+
+const btnCompartir = document.querySelector(".btn-compartir");
+const shareCard = document.querySelector(".share-card");
+
+shareCard
+  .querySelector(".close")
+  .addEventListener("click", () => shareCard.classList.remove("show"));
+
+shareCard.addEventListener("click", (event) => {
+  if (event.target == shareCard.querySelector(".share-card-background")) {
+    shareCard.classList.remove("show");
+  }
+});
+
+btnCompartir.addEventListener("click", () => shareCard.classList.add("show"));
