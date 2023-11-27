4d9eac4da54ed5532da99be245e1b049cefd99e6
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2023-11-26 21:10:45 -0300

    add parallax scene1

--- TP4/EntregaFinal/js/animations.js
+++ TP4/EntregaFinal/js/animations.js
@@ -4,7 +4,7 @@ window.onload = () => {
     const imgLogo = document.querySelector('.logo');
     const sceneOneSpiderLeft = document.querySelector('.scene-one-spider-left');
     const sceneOneSpiderMiddle = document.querySelector('.scene-one-spider-middle');
-    const scenneOneSpiderRight = document.querySelector('.scene-one-spider-right');
+    const sceneOneSpiderRight = document.querySelector('.scene-one-spider-right');
 
     window.addEventListener("scroll", (event) => {
         const scrollY = window.scrollY;
@@ -13,8 +13,25 @@ window.onload = () => {
         } else {
             imgLogo.classList.remove('logo-menu');
         }
-
+        
+        const scene1MaxY = 200;
+        const scene1scrollY = Math.min(scrollY, scene1MaxY);
+        const scene1Variation = scene1scrollY / scene1MaxY;
         // Parallax effect for scene 1 spidermen
-        // sceneOneSpiderLeft.style.transform = `translateY(${scrollY * 0.3}px)`;
+        sceneOneSpiderLeft.style.transform = `scale(${1 + .5 * scene1Variation})
+                                              translateX(${scene1Variation * -200}px)
+                                              translateY(${scene1Variation * -100}px)
+                                              `;
+        sceneOneSpiderMiddle.style.transform = `scale(${1 + .6 * scene1Variation}) 
+                                                translateX(${scene1Variation * -50}px)
+                                                translateY(${scene1Variation * 0}px)
+                                                rotate(${10 * scene1Variation}deg)`;
+        sceneOneSpiderRight.style.transform = ` scale(${1 + .3 * scene1Variation})
+                                                translateX(${scene1Variation * 100}px)
+                                                translateY(${scene1Variation * 100}px)
+                                                rotate(${-10 * scene1Variation}deg)`;
     })
+
+
+
 }
