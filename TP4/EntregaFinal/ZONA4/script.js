fcdbe7a45a479353294ecaa7ae0aa4aaaeb1b322
valentin-charlone <valen-charlone@hotmail.com>
2023-11-27 00:15:45 -0300

    minor changes

--- TP4/EntregaFinal/ZONA4/script.js
+++ TP4/EntregaFinal/ZONA4/script.js
@@ -8,18 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
     let textSticky2 =  document.querySelector("#text2");
     let textSticky3 =  document.querySelector("#text3");
     let textSticky4 =  document.querySelector("#text4");
+
     document.addEventListener('scroll', () => {
-        if (window.scrollY < 500) {
+        const scrollY = window.scrollY;
+        
+        if (scrollY < 500) {
             imgSticky1.style.opacity = 0;
             textSticky1.style.opacity = 0;
         }
-        else if (window.scrollY > 500 && window.scrollY < 1000) {
+        else if (scrollY > 500 && scrollY < 1000) {
             imgSticky1.style.opacity = 1;
             textSticky1.style.opacity = 1;
             imgSticky2.style.opacity = 0;
             textSticky2.style.opacity = 0;
         }
-        else if (window.scrollY > 1000 && window.scrollY < 1600) {
+        else if (scrollY > 1000 && scrollY < 1600) {
             imgSticky1.style.opacity = 0;
             textSticky1.style.opacity = 0;
             imgSticky2.style.opacity = 1;
@@ -27,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
             imgSticky3.style.opacity = 0;
             textSticky3.style.opacity = 0;
         }
-        else if (window.scrollY > 1600 && window.scrollY < 2200) {
+        else if (scrollY > 1600 && scrollY < 2200) {
             imgSticky2.style.opacity = 0;
             textSticky2.style.opacity = 0;
             imgSticky3.style.opacity = 1;
@@ -35,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
             imgSticky4.style.opacity = 0;
             textSticky4.style.opacity = 0;
         }
-        else if (window.scrollY > 2200 && window.scrollY < 2700) {
+        else if (scrollY > 2200 && scrollY < 2700) {
             imgSticky3.style.opacity = 0;
             textSticky3.style.opacity = 0;
             imgSticky4.style.opacity = 1;
