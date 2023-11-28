1be97059b9b55b3a3fca55ad0d48c88a395c5694
MatiasPViecho <mpviecho@gmail.com>
2023-11-27 21:34:47 -0300

    feat: add menu header links with animation

--- TP4/EntregaFinal/js/menu.js
+++ TP4/EntregaFinal/js/menu.js
@@ -1,13 +1,16 @@
 "use strict"
-console.log("loaded");
 let flag = false;
 
 const headerBtn = document.querySelector('#header-menu-button');
+const headerMenu = document.querySelector('#menu-header-options');
+
 headerBtn.addEventListener('click', () => {
     if(!flag){
         headerBtn.classList.add('animate');
+        headerMenu.classList.add('open');
     } else {
         headerBtn.classList.remove('animate');
+        headerMenu.classList.remove('open');
     }
     flag = !flag;
 })
